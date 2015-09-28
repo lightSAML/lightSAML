6efc16fbbf52716db30fe29951364dcb7b505626
Milos Tomic <tmilos@gmail.com>
2015-09-28 11:41:46 +0200

    FooTest fix

--- src/LightSaml/Tests/Functional/Model/Protocol/AuthnRequestFunctionalTest.php
+++ src/LightSaml/Tests/Functional/Model/Protocol/AuthnRequestFunctionalTest.php
@@ -2,8 +2,14 @@
 
 namespace LightSaml\Tests\Functional\Model\Protocol;
 
+use LightSaml\Model\Assertion\Issuer;
 use LightSaml\Model\Context\DeserializationContext;
+use LightSaml\Model\Context\SerializationContext;
 use LightSaml\Model\Protocol\AuthnRequest;
+use LightSaml\Model\Security\KeyHelper;
+use LightSaml\Model\Security\X509Certificate;
+use LightSaml\Model\XmlDSig\SignatureWriter;
+use LightSaml\Model\XmlDSig\SignatureXmlReader;
 use LightSaml\SamlConstants;
 
 class AuthnRequestFunctionalTest extends \PHPUnit_Framework_TestCase
@@ -29,5 +35,52 @@ class AuthnRequestFunctionalTest extends \PHPUnit_Framework_TestCase
         $this->assertNotNull($request->getNameIDPolicy());
         $this->assertEquals(SamlConstants::NAME_ID_FORMAT_PERSISTENT, $request->getNameIDPolicy()->getFormat());
         $this->assertTrue($request->getNameIDPolicy()->getAllowCreate());
+    }
+
+    public function testSignedSerializeDeserialize()
+    {
+        $certificate = new X509Certificate();
+        $certificate->loadFromFile(__DIR__.'/../../../../../../web/sp/saml.crt');
+        $privateKey = KeyHelper::createPrivateKey(__DIR__.'/../../../../../../web/sp/saml.key', null, true);
+
+        $authnRequest = new AuthnRequest();
+        $authnRequest->setID('_894da3368874d2dd637983b6812f66c444f100f205');
+        $authnRequest->setIssueInstant('2015-09-13T11:47:33Z');
+        $authnRequest->setDestination('https://idp.testshib.org/idp/profile/SAML2/POST/SSO');
+        $authnRequest->setIssuer(
+            (new Issuer())
+                ->setValue('https://mt.evo.loc/sp')
+                ->setFormat('urn:oasis:names:tc:SAML:2.0:nameid-format:entity')
+        );
+        $authnRequest->setSignature(
+            new SignatureWriter($certificate, $privateKey)
+        );
+
+        $serializationContext = new SerializationContext();
+        $authnRequest->serialize($serializationContext->getDocument(), $serializationContext);
+
+        $temporaryFilename = tempnam(sys_get_temp_dir(), 'lightsaml-');
+
+        $serializationContext->getDocument()->save($temporaryFilename);
+
+
+        $xml = file_get_contents($temporaryFilename);
+        $deserializationContext = new DeserializationContext();
+        $deserializationContext->getDocument()->loadXML($xml);
+
+        $authnRequest = new AuthnRequest();
+        $authnRequest->deserialize($deserializationContext->getDocument()->firstChild, $deserializationContext);
+
+        $signatureReader = $authnRequest->getSignature();
+        if ($signatureReader instanceof SignatureXmlReader) {
+            $certificate = new X509Certificate();
+            $certificate->loadFromFile(__DIR__.'/../../../../../../web/sp/saml.crt');
+            $key = KeyHelper::createPublicKey($certificate);
+            $ok = $signatureReader->validate($key);
+            $this->assertTrue($ok);
+        } else {
+            throw new \LogicException('Expected Signature Xml Reader');
+        }
+
     }
 }
