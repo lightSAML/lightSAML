cac80c9deea6c03d1bacfb89a26be7a283b3eda0
Milos Tomic <tmilos@gmail.com>
2017-01-23 08:41:04 +0100

    test relay state from saml is included in http post #71

--- tests/LightSaml/Tests/Binding/HttpPostBindingTest.php
+++ tests/LightSaml/Tests/Binding/HttpPostBindingTest.php
@@ -2,8 +2,12 @@
 
 namespace LightSaml\Tests\Binding;
 
+use LightSaml\Binding\BindingFactory;
 use LightSaml\Binding\HttpPostBinding;
 use LightSaml\Context\Profile\MessageContext;
+use LightSaml\Model\Protocol\Response;
+use LightSaml\SamlConstants;
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpFoundation\Request;
 
 class HttpPostBindingTest extends \PHPUnit_Framework_TestCase
@@ -12,7 +16,7 @@ class HttpPostBindingTest extends \PHPUnit_Framework_TestCase
      * @expectedException \LightSaml\Error\LightSamlBindingException
      * @expectedExceptionMessage Missing SAMLRequest or SAMLResponse parameter
      */
-    public function test__receive_throws_when_no_message()
+    public function test_receive_throws_when_no_message()
     {
         $request = new Request();
 
@@ -21,5 +25,31 @@ class HttpPostBindingTest extends \PHPUnit_Framework_TestCase
         $messageContext = new MessageContext();
 
         $binding->receive($request, $messageContext);
+    }
+
+    public function test_relay_state_is_included_in_http_post()
+    {
+        $expectedRelayState = 'some_relay_state';
+
+        $samlResponse = new Response();
+        $samlResponse->setRelayState($expectedRelayState);
+
+        $messageContext = new MessageContext();
+        $messageContext->setMessage($samlResponse);
+
+        $this->assertEquals($expectedRelayState, $messageContext->getMessage()->getRelayState());
+
+        $bindingFactory = new BindingFactory();
+        $binding = $bindingFactory->create(SamlConstants::BINDING_SAML2_HTTP_POST);
+
+        $httpResponse = $binding->send($messageContext);
+
+        $html = $httpResponse->getContent();
+
+        $crawler = new Crawler($html);
+        $relayStateInputs = $crawler->filter('body form input[name="RelayState"]');
+        $this->assertEquals(1, $relayStateInputs->count());
+        $actualRelayState = $relayStateInputs->first()->attr('value');
+        $this->assertEquals($expectedRelayState, $actualRelayState);
     }
 }
