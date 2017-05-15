88b1bf5adf1def31705014527f970ea1209589e8
Francis Lavoie <lavofr@gmail.com>
2017-05-15 14:59:18 -0400

    Add Status message setter test

---
+++ tests/LightSaml/Tests/Model/Protocol/StatusTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace LightSaml\Tests\Model\Protocol;
+
+use LightSaml\Model\Protocol\Status;
+use LightSaml\SamlConstants;
+
+class ResponseTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_status_set_message_constructor()
+    {
+        $message = "Test message";
+        $status = new Status(new StatusCode(SamlConstants::STATUS_SUCCESS), $message);
+        $this->assertEquals($status->getStatusMessage(), $message);
+    }
+    
+    public function test_status_set_message_setter()
+    {
+        $message = "Test message";
+        $status = new Status(new StatusCode(SamlConstants::STATUS_SUCCESS));
+        $status->setStatusMessage($message);
+        $this->assertEquals($status->getStatusMessage(), $message);
+    }
+}
