e1307762931eb296c1a813eb7df9f96ecb6e57dd
Josh Jarvis <josh@abgrm.com>
2020-06-03 12:09:45 -0600

    Added a line to EncryptedElementWriter->encrypt() which will preserve the key name across key reloading.

--- src/LightSaml/Model/Assertion/EncryptedElementWriter.php
+++ src/LightSaml/Model/Assertion/EncryptedElementWriter.php
@@ -50,6 +50,7 @@ abstract class EncryptedElementWriter extends EncryptedElement
         $oldKey = $key;
         $key = new XMLSecurityKey($this->keyTransportEncryption, ['type' => 'public']);
         $key->loadKey($oldKey->key);
+        $key->name = $oldKey->name;
 
         $serializationContext = new SerializationContext();
         $object->serialize($serializationContext->getDocument(), $serializationContext);
