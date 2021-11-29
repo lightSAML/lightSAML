cfab424eca984d63acc2e89b529caf049eb705d1
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2021-11-29 15:17:33 +0100

    style: fix typo in autoload.php

--- autoload.php
+++ autoload.php
@@ -3,6 +3,6 @@
 // if library is in dev environement with its own vendor, include its autoload
 if(file_exists(__DIR__ . '/vendor'))
     require_once __DIR__ . '/vendor/autoload.php';
-// if library is in vendor of another project, include the global autolaod
+// if library is in vendor of another project, include the global autoload
 else
     require_once __DIR__ . '/../../autoload.php';
